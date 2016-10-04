23752e3456e52ad4b1e7d7fffb7b6d87fafd906a
semlie <israellieb@gmail.com>
2016-10-04 23:50:00 +0300

    add print hiden divs

--- header.php
+++ header.php
@@ -23,11 +23,20 @@
 
         <!-- Custom Fonts -->
         <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <style type="text/css" >
+            @media print {
+                .no-print, .no-print *
+                {
+                    display: none !important;
+                }
+            }
+        </style>
         <style type="text/css" media="print">
             @page {
                 size: auto;   /* auto is the initial value */
                 margin: 0;  /* this affects the margin in the printer settings */
             }
+
         </style>
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -41,20 +50,8 @@
     <body>
 
         <div id="wrapper">
-            
-            <!-- Navigation -->
-            <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="index.html">ניהול הזמנות</a>
-                </div>
-                <!-- /.navbar-header -->
-
+            <div class="no-print">
+                <!-- Navigation -->
                 <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
                     <div class="navbar-header">
                         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
@@ -63,44 +60,57 @@
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-                        <a class="navbar-brand" href="index.html">סוכות</a>
+                        <a class="navbar-brand" href="index.html">ניהול הזמנות</a>
                     </div>
                     <!-- /.navbar-header -->
 
-                    <ul class="nav navbar-top-links navbar-right">
+                    <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <a class="navbar-brand" href="index.html">סוכות</a>
+                        </div>
+                        <!-- /.navbar-header -->
 
-                        <!-- /.dropdown -->
+                        <ul class="nav navbar-top-links navbar-right">
 
-                        <!-- /.dropdown -->
-                    </ul>
+                            <!-- /.dropdown -->
+
+                            <!-- /.dropdown -->
+                        </ul>
+                        <!-- /.navbar-top-links -->
+
+                        <div class="navbar-default sidebar" role="navigation">
+                            <div class="sidebar-nav navbar-collapse">
+                                <ul class="nav" id="side-menu">
+
+                                    <li>
+                                        <a href="orders.php"><i class="fa fa-dashboard fa-fw"></i> Orders</a>
+                                    </li>
+                                    <li>
+                                        <a href="callers.php"><i class="fa fa-dashboard fa-fw"></i> Callers</a>
+                                    </li>
+                                    <li>
+                                        <a href="products.php"><i class="fa fa-dashboard fa-fw"></i>Products </a>
+                                    </li>
+                                    <li>
+                                        <a href="productsreport.php"><i class="fa fa-dashboard fa-fw"></i> Products sales report</a>
+                                    </li>
+
+
+                                </ul>
+                            </div>
+                            <!-- /.sidebar-collapse -->
+                        </div>
+                        <!-- /.navbar-static-side -->
+                    </nav>
                     <!-- /.navbar-top-links -->
 
-                    <div class="navbar-default sidebar" role="navigation">
-                        <div class="sidebar-nav navbar-collapse">
-                            <ul class="nav" id="side-menu">
 
-                                <li>
-                                    <a href="orders.php"><i class="fa fa-dashboard fa-fw"></i> Orders</a>
-                                </li>
-                                <li>
-                                    <a href="callers.php"><i class="fa fa-dashboard fa-fw"></i> Callers</a>
-                                </li>
-                                <li>
-                                    <a href="products.php"><i class="fa fa-dashboard fa-fw"></i>Products </a>
-                                </li>
-                                <li>
-                                    <a href="productsreport.php"><i class="fa fa-dashboard fa-fw"></i> Products sales report</a>
-                                </li>
-                                
-
-                            </ul>
-                        </div>
-                        <!-- /.sidebar-collapse -->
-                    </div>
                     <!-- /.navbar-static-side -->
                 </nav>
-                <!-- /.navbar-top-links -->
-
-
-                <!-- /.navbar-static-side -->
-            </nav>
+            </div>
--- orderdetails.php
+++ orderdetails.php
@@ -16,141 +16,144 @@ function discountPrice($price) {
 <?php require_once 'header.php';
 ?>
 
-            <div id="page-wrapper">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">הזמנה מספר - 102<?php echo $data->OrderId; ?></h1>
-                    </div>
-                    <!-- /.col-lg-12 -->
-                </div>
-                <!-- /.row -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                פרטי המזמין
-                            </div>
-                            <!-- /.panel-heading -->
-                            <div class="panel-body"><div class="table-responsive">
-                                    <table class="table table-striped table-bordered table-hover" id="dataTables-example">
-                                        <thead>
-                                            <tr>
-                                                <th>OrderId</th>
-                                                <th>Region</th>
-                                                <th>Name</th>
-                                                <th>Address</th>
-                                                <th>City</th>
-                                                <th>PhoneNumber</th>
-                                                <th>OtherPhone</th>
-                                                <th>TotalQuantity</th>
-                                                <th>TotalPrice</th>
-                                                <th>TotalItems</th>
-                                                <th>edit</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-
-                                            <tr class="odd gradeX">
-                                                <td><?php echo $data->OrderId; ?></td>
-                                                <td><?php echo $data->Region; ?></td>
-                                                <td><?php echo $data->Name; ?></td>
-                                                <td><?php echo $data->Address; ?></td>
-                                                <td><?php echo $data->City; ?></td>
-                                                <td><?php echo $data->PhoneNumber; ?></td>
-                                                <td><?php echo $data->OtherPhone; ?></td>
-                                                <td><?php echo $data->TotalQuantity; ?></td>
-                                                <td><?php
-                                                    echo $data->TotalPrice;
-                                                    echo discountPrice($data->TotalPrice)
-                                                    ?></td>
-                                                <td><?php echo $data->TotalItems; ?></td>
-                                                <td><a href="editorders.php?orderid=<?php echo $data->OrderId; ?>"> Edit</a> </td>
-
-                                            </tr>
-
-
-
-                                        </tbody>
-                                    </table>
-                                    <table class="table table-striped table-bordered table-hover" id="dataTables-example">
-                                        <tbody>
-                                            <tr class="odd gradeX">
-                                                <td><?php echo $data->Notes; ?></td>
-
-                                            </tr>
-
-                                        </tbody>
-                                    </table>
-
-                                </div>
-                                <!-- /.table-responsive -->
-
-                            </div>
-                            <!-- /.panel-body -->
-                        </div>
-                        <!-- /.panel -->
-                    </div>
-                    <!-- /.col-lg-12 -->
-                </div>
-                <!-- /.row -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                פרטי הזמנה
-                            </div>
-                            <!-- /.panel-heading -->
-                            <div class="panel-body">
-                                <div class="table-responsive">
-                                    <table class="table table-striped table-bordered table-hover">
-                                        <thead>
-                                            <tr>
-                                                <th>#</th>
-                                                <th>ProductId</th>
-                                                <th>ProductName</th>
-                                                <th>Quantity</th>
-                                                <th>PriceUnit</th>
-                                                <th>Price</th>
-                                                <th>edit</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <?php
-                                            foreach ($orderItems as $value) {
-                                                $counter++;
-                                                ?>
-                                                <tr>
-                                                    <td><?php echo $counter; ?></td>
-                                                    <td><?php echo $value->ProductId; ?></td>
-                                                    <td><?php echo $value->ProductName; ?></td>
-                                                    <td><?php echo $value->Quantity; ?></td>
-                                                    <td><?php echo $value->PriceUnit; ?></td>
-                                                    <td><?php echo $value->PriceOrderItem; ?></td>
-                                                    <td><a href="editorderitems.php?orderitemid=<?php echo $value->Id; ?>">Edit</a></td>
-
-                                                </tr>
-<?php } ?>
-                                        </tbody>
-                                    </table>
-                                </div>
-                                <a href="addorderItems.php?orderid=<?php echo $data->OrderId; ?>" class="btn btn-info" role="button">Add New Item To Order</a>
-
-                                <!-- /.table-responsive -->
-                            </div>
-                            <!-- /.panel-body -->
-                        </div>
-                        <!-- /.panel -->
-                    </div>
-                    <!-- /.col-lg-6 -->
-                    <!-- /.col-lg-6 -->
-                </div>
-                <!-- /.row -->
-                <!-- /.row -->
-                <!-- /.row -->
-            </div>
-            <!-- /#page-wrapper -->
-
+<div id="page-wrapper">
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">הזמנה מספר - 102<?php echo $data->OrderId; ?></h1>
         </div>
-        <!-- /#wrapper -->
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    פרטי המזמין
+                </div>
+                <!-- /.panel-heading -->
+                <div class="panel-body"><div class="table-responsive">
+                        <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                            <thead>
+                                <tr>
+                                    <th class="no-print">OrderId</th>
+                                    <th class="no-print">Region</th>
 
-<?php            require_once 'footer.php';
+                                    <th>Name</th>
+                                    <th>Address</th>
+                                    <th>City</th>
+                                    <th>Phone1</th>
+                                    <th>Phone2</th>
+                                    <th>Quantity</th>
+                                    <th>Price</th>
+                                    <th>TotalItems</th>
+                                    <th class="no-print">edit</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                                <tr class="odd gradeX">
+                                    <td class="no-print"><?php echo $data->OrderId; ?></td>
+                                    <td class="no-print"><?php echo $data->Region; ?></td>
+                                    <td><?php echo $data->Name; ?></td>
+                                    <td><?php echo $data->Address; ?></td>
+                                    <td><?php echo $data->City; ?></td>
+                                    <td><?php echo $data->PhoneNumber; ?></td>
+                                    <td><?php echo $data->OtherPhone; ?></td>
+                                    <td><?php echo $data->TotalQuantity; ?></td>
+                                    <td><?php
+                                        echo $data->TotalPrice;
+                                        echo discountPrice($data->TotalPrice)
+                                        ?></td>
+                                    <td><?php echo $data->TotalItems; ?></td>
+                                    <td class="no-print"><a href="editorders.php?orderid=<?php echo $data->OrderId; ?>"> Edit</a> </td>
+
+                                </tr>
+
+
+
+                            </tbody>
+                        </table>
+                        <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                            <tbody>
+                                <tr class="odd gradeX">
+                                    <td><?php echo $data->Notes; ?></td>
+
+                                </tr>
+
+                            </tbody>
+                        </table>
+
+                    </div>
+                    <!-- /.table-responsive -->
+
+                </div>
+                <!-- /.panel-body -->
+            </div>
+            <!-- /.panel -->
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    פרטי הזמנה
+                </div>
+                <!-- /.panel-heading -->
+                <div class="panel-body">
+                    <div class="table-responsive">
+                        <table class="table table-striped table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>ProductId</th>
+                                    <th>ProductName</th>
+                                    <th>Quantity</th>
+                                    <th>PriceUnit</th>
+                                    <th>Price</th>
+                                    <th class="no-print">edit</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                foreach ($orderItems as $value) {
+                                    $counter++;
+                                    ?>
+                                    <tr>
+                                        <td><?php echo $counter; ?></td>
+                                        <td><?php echo $value->ProductId; ?></td>
+                                        <td><?php echo $value->ProductName; ?></td>
+                                        <td><?php echo $value->Quantity; ?></td>
+                                        <td><?php echo $value->PriceUnit; ?></td>
+                                        <td><?php echo $value->PriceOrderItem; ?></td>
+                                        <td class="no-print"><a href="editorderitems.php?orderitemid=<?php echo $value->Id; ?>">Edit</a></td>
+
+                                    </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="no-print">  
+                        <a href="addorderItems.php?orderid=<?php echo $data->OrderId; ?>" class="btn btn-info" role="button">Add New Item To Order</a>
+                    </div>
+                    <!-- /.table-responsive -->
+                </div>
+                <!-- /.panel-body -->
+            </div>
+            <!-- /.panel -->
+        </div>
+        <!-- /.col-lg-6 -->
+        <!-- /.col-lg-6 -->
+    </div>
+    <!-- /.row -->
+    <!-- /.row -->
+    <!-- /.row -->
+</div>
+<!-- /#page-wrapper -->
+
+</div>
+<!-- /#wrapper -->
+
+<?php
+require_once 'footer.php';
